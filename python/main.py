ddd89f6433b4c95deadedc80e9789aa865720eb4
romaindasilva <romain.dasilva@lachouette.co>
2023-10-22 15:23:43 +0200

    update main

--- python/main.py
+++ python/main.py
@@ -1,100 +1,78 @@
+import time
 from perceptron import ActivationType
-from dense import Dense
+from layer import Layer
 
-"""
- This is a simple dataset to test the perceptron
- First value is the temperature, the seconde is the humidity and the last one
- is the wind speed
- The expected value is 1.0 if it's good condition to wearing a jacket, 0.0
- otherwise
+# from layer import Layer
 
- dataset = [
-    # Temperature, rain risk, wind | response
-    ([2, 60, 20], 1.0),
-    ([5, 30, 40], 1.0),
-    ([8, 40, 10], 1.0),
-    ([12, 80, 10], 1.0),
-    ([9, 0, 50], 1.0),
-    ([23, 10, 10], 0.0),  # Used for testing
-    ([20, 0, 40], 0.0),
-    ([19, 5, 20], 0.0),
-    ([22, 10, 40], 0.0),
-    ([29, 0, 0], 0.0),
-]
-"""
-dataset: [([float], float)] = [
-    ([2, 60, 20], 1.0),
-    ([2, 70, 10], 1.0),
-    ([2, 80, 15], 1.0),
-    ([12, 50, 35], 1.0),
-    ([12, 30, 80], 1.0),
-    #
-    ([40, 10, 20], 0.0),
-    ([35, 10, 0], 0.0),
-    ([28, 5, 20], 0.0),
-    ([32, 10, 5], 0.0),
-    ([30, 15, 2], 0.0),
-]
+if __name__ == "__main__":
+    start = time.time()
 
-epoch = 10000
+    dataset = [
+        ([0.12, 0.90, 0.10], 1),
+        ([0.10, 0.70, 0.40], 1),
+        ([0.14, 0.40, 0.20], 1),
+        ([0.01, 0.60, 0.60], 1),
+        ([0.08, 0.50, 0.30], 1),
+        #
+        ([0.25, 0.10, 0.10], 0),
+        ([0.30, 0.15, 0.10], 0),
+        ([0.28, 0.05, 0.30], 0),
+        ([0.21, 0.0, 0.0], 0),
+        ([0.40, 0.10, 0.10], 0),
+    ]
 
-dense = Dense(
-    nbInput=3,
-    nbHidden=6,
-    hiddenAct=ActivationType.RELU,
-    nbOutput=1,
-    outputAct=ActivationType.SIGMOID,
-)
+    epoch = 100
 
-for e in range(epoch):
-    meanError = 0.0
-    for d in dataset:
-        # Prediction
-        readyInput = [d[0][0] / 100.0, d[0][1] / 100.0, d[0][2] / 100.0]
+    allErrors = []
 
-        pred = dense.forward(inputs=readyInput)
-        # Uncomment to print weights before and after training
-        # print("Before training")
-        # print([p.weights for p in dense.hiddenLayer])
-        # print([p.weights for p in dense.outLayer])
-        # print(pred)
-        # Calc Error (delta)
-        error = d[1] - pred[0]
-        dense.backward(deltas=[error])
-        # Uncomment to print weights after training
-        # print("After training")
-        # print([p.weights for p in dense.hiddenLayer])
-        # print([p.weights for p in dense.outLayer])
-        meanError += abs(error)
-    meanError /= len(dataset)
-    print(meanError)
+    """
 
+    for i in range(1000):
+        p = Perceptron(3, ActivationType.THRESHOLD)
 
-"""
+        errorMean = 0
+        for inputs, target in dataset:
+            errorMean += abs(target - p.predict(inputs))
+        errorMean /= len(dataset)
 
-# print('perceptron ...')
-p = Perceptron(3, ActivationType.TRESHOLD)
-epoch = 1000
+        #  print(f"error mean before training: {errorMean}")
+        # print("training...")
 
-print("before training")
+        for e in range(epoch):
+            for inputs, target in dataset:
+                p.train(inputs, target)
 
-pred = p.forward([2.0, 3.0, 4.0])
-print(pred)
+        errorMean = 0
+        for inputs, target in dataset:
+            errorMean += abs(target - p.predict(inputs))
+        errorMean /= len(dataset)
 
-print("training ...")
+        # print(f"error mean after training: {errorMean}")
 
-for e in range(epoch):
-    currentEpochError = 0.0
+        allErrors.append(errorMean)
 
-    for i in dataset:
-        currentEpochError += p.train(i[0], i[1])
+    print("Moyenne des erreurs: ", sum(allErrors) / len(allErrors))
 
-    meanError = currentEpochError / len(dataset)
+    """
 
-    print(meanError)
+    # mean error before training
 
-print("after training")
-print(p.weights)
+    for e in range(500):
+        layer = Layer(
+            3, (9, ActivationType.RELU), (1, ActivationType.THRESHOLD)
+        )
 
+        for e in range(epoch):
+            for inputs, target in dataset:
+                layer.train(inputs, [target])
 
-"""
+        errorMean = 0
+        for inputs, target in dataset:
+            errorMean += abs(target - layer.predict(inputs)[0])
+        errorMean /= len(dataset)
+
+        allErrors.append(errorMean)
+
+    print("Moyenne des erreurs: ", sum(allErrors) / len(allErrors))
+
+    print(f"Temps écoulé: {time.time() - start} secondes")
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,37 +1,55 @@
-extern crate rand;
+#![allow(warnings)]
+
+use std::time::Instant;
 mod perceptron;
-mod dense;
-use perceptron::{ActivationType};
-use dense::{Dense};
+mod layer;
+use crate::perceptron::ActivationType;
+use crate::layer::Layer;
+
 
 fn main() {
-    /*  This is a simple dataset to test the perceptron
-        First value is the temperature, the seconde is the humidity and the last one
-        is the wind speed
-        The expected value is 1.0 if it's good condition to wearing a jacket, 0.0
-        otherwise
-    */
+    let start = Instant::now();
 
-    let dataset: [([f64; 3], f64); 10] = [
-        ([2.0, 60.0, 20.0], 1.0),
-        ([2.0, 70.0, 10.0], 1.0),
-        ([2.0, 80.0, 15.0], 1.0),
-        ([12.0, 50.0, 35.0], 1.0),
-        ([12.0, 30.0, 80.0], 1.0),
-        //
-        ([40.0, 10.0, 20.0], 0.0),
-        ([35.0, 10.0, 0.0], 0.0),
-        ([28.0, 5.0, 20.0], 0.0),
-        ([32.0, 10.0, 5.0], 0.0),
-        ([30.0, 15.0, 2.0], 0.0),
+    let dataset: Vec<([f64; 3], i32)> = vec![
+        ([0.12, 0.90, 0.10], 1),
+        ([0.10, 0.70, 0.40], 1),
+        ([0.14, 0.40, 0.20], 1),
+        ([0.01, 0.60, 0.60], 1),
+        ([0.08, 0.50, 0.30], 1),
+        ([0.25, 0.10, 0.10], 0),
+        ([0.30, 0.15, 0.10], 0),
+        ([0.28, 0.05, 0.30], 0),
+        ([0.21, 0.0, 0.0], 0),
+        ([0.40, 0.10, 0.10], 0),
     ];
 
-    // Création d'une instance de Dense
-    let mut dense = Dense::new(3, 2, ActivationType::RELU, 2, ActivationType::SIGMOID);
+    let epoch = 100;
+    let mut all_errors = Vec::new();
 
-    // Faire une prédiction
-    let pred = dense.forward(vec![1.0, 2.0, 3.0]);
+    for _ in 0..500 {
+        let mut layer = Layer::new(
+            3,
+            (9, ActivationType::Relu),
+            (1, ActivationType::Threshold),
+        );
 
-    // Print la prédiction
-    println!("{:?}", pred);
+        for _ in 0..epoch {
+            for (inputs, target) in &dataset {
+                layer.train(&inputs.to_vec(), &vec![*target as f64]);
+            }
+        }
+
+        let mut error_mean = 0.0;
+        for (inputs, target) in &dataset {
+            error_mean += (*target as f64 - layer.predict(&inputs.to_vec())[0]).abs();
+        }
+        error_mean /= dataset.len() as f64;
+
+        all_errors.push(error_mean);
+    }
+
+    let error_avg: f64 = all_errors.iter().sum::<f64>() / all_errors.len() as f64;
+    println!("Moyenne des erreurs: {}", error_avg);
+
+    println!("Temps écoulé: {} secondes", start.elapsed().as_secs_f64());
 }
